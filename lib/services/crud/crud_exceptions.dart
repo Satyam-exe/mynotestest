df56475d489bce70f641d9b002d6366fd05fecff
Satyam Vashisht <satyam2007@gmail.com>
2023-02-25 22:05:40 +0530

    added crud

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class DatabaseNotOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectoryException implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class UserDoesntExistException implements Exception {}
+
+class NoteDoesntExistException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
